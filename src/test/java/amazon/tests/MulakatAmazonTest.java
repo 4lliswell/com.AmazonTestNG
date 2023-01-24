115580ac5706bef8cc40ddd370be8329ce0dda1d
TheHatipoglu <ciftciyunusemre20@gmail.com>
2023-01-24 19:53:07 +0300

    24.01.23

--- src/test/java/amazon/tests/MulakatAmazonTest.java
+++ src/test/java/amazon/tests/MulakatAmazonTest.java
@@ -89,7 +89,7 @@ public class MulakatAmazonTest extends TestBaseReport {
         //15. “Favori Listem” sayfası açıldığı kontrol edilir.
         String favoriListemUrl = Driver.getDriver().getCurrentUrl();
         Assert.assertTrue(favoriListemUrl.contains("wishlist"));
-        extentTest = extentReports.createTest("Favori Listem Sayfasının Açıldığı Kontrol Edildi");
+        extentTest("Favori Listem Sayfasının Açıldığı Kontrol Edildi");
 
         //16. Eklenen ürün favorilerden silinir.
         amazonPage.deleteButon.click();
