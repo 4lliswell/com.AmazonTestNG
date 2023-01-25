f398958ede58bf18d3f933c8a5388a857af833d7
TheHatipoglu <ciftciyunusemre20@gmail.com>
2023-01-25 15:29:15 +0300

    25.01.23

--- src/test/java/amazon/tests/MulakatAmazonTest.java
+++ src/test/java/amazon/tests/MulakatAmazonTest.java
@@ -97,7 +97,7 @@ public class MulakatAmazonTest extends TestBaseReport {
 
         //17. Silme işleminin gerçekleştiği kontrol edilir.
         Assert.assertTrue(amazonPage.silindi.isDisplayed());
-        extentTest("Silme İşleminin Gerçekleştiği Kontrol Edildi");
+        extentTest.info("Silme İşleminin Gerçekleştiği Kontrol Edildi");
 
         //18. Üye çıkış işlemi yapılır.
         ReusableMethods.moveToElementHover(amazonPage.txtGirisYapinPencere);
